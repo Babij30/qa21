c1d6cbb7ab9a31745ec90a3466ef4bc8c19ebd12
Babij30 <2mcs.au@gmail.com>
2023-12-23 14:05:47 -0700

    function

---
+++ function.js
@@ -0,0 +1,40 @@
+function addTwonumber(num1,num2)
+          {
+            result = num1 + num2
+            console.log(result)
+        }
+
+let myfunc =   (num1,num2) => {
+      result = num1 + num2
+      console.log(result)
+      }
+
+myfunc(10,20)
+
+
+let a = function (num)
+        {
+              if(num % 2 == 0)
+              {
+                return "even"
+              }
+              else
+              {
+                return "odd"
+              }
+        }
+
+result=a(11)
+console.log(result)
+
+let n1 = 100
+console.log(n1)
+function test()
+     {
+       n1 = 10
+       
+     }
+test()
+console.log(n1)     
+
+
